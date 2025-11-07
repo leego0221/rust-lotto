da1f510ca62f43af19563b992f2df8c5c80aa436
leego0221 <leego0221@naver.com>
2025-11-07 15:04:10 +0900

    first commit

---
+++ target/debug/deps/rust_lotto-2ec8ae135d1b670d.d
@@ -0,0 +1,5 @@
+C:\Users\leego\Desktop\RustProjects\rust-lotto\target\debug\deps\rust_lotto-2ec8ae135d1b670d.d: src\main.rs
+
+C:\Users\leego\Desktop\RustProjects\rust-lotto\target\debug\deps\librust_lotto-2ec8ae135d1b670d.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/rust_lotto-8b0b822d12b23276.d
@@ -0,0 +1,5 @@
+C:\Users\leego\Desktop\RustProjects\rust-lotto\target\debug\deps\rust_lotto-8b0b822d12b23276.d: src\main.rs
+
+C:\Users\leego\Desktop\RustProjects\rust-lotto\target\debug\deps\librust_lotto-8b0b822d12b23276.rmeta: src\main.rs
+
+src\main.rs:
