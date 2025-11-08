f00b88c52c19dd2decf90fa39c3fd270d7806d01
leego0221 <leego0221@naver.com>
2025-11-08 17:21:20 +0900

    chore: .gitignore 설정

--- target/debug/deps/rust_lotto-2ec8ae135d1b670d.d
+++
@@ -1,5 +0,0 @@
-C:\Users\leego\Desktop\RustProjects\rust-lotto\target\debug\deps\rust_lotto-2ec8ae135d1b670d.d: src\main.rs
-
-C:\Users\leego\Desktop\RustProjects\rust-lotto\target\debug\deps\librust_lotto-2ec8ae135d1b670d.rmeta: src\main.rs
-
-src\main.rs:
