0a8bf48ff7837aa2e2a8c47ccc4ed941c163a043
leego0221 <leego0221@naver.com>
2025-11-09 23:19:20 +0900

    feat: 구입 금액 입력

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("구입금액을 입력해 주세요.");
+
+    let mut purchase_amount = String::new();
+
+    io::stdin()
+        .read_line(&mut purchase_amount)
+        .expect("구입금액 읽기에 실패했습니다.");
+
+    println!("[DEBUG] 구입금액: {purchase_amount}");
 }
