0b4e6b9bfdb1438826d99e55ce86110df20637c5
leego0221 <leego0221@naver.com>
2025-11-09 23:22:05 +0900

    feat: 당첨 번호와 보너스 번호 입력

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,23 @@ fn main() {
         .read_line(&mut purchase_amount)
         .expect("구입금액 읽기에 실패했습니다.");
 
+    println!("당첨 번호를 입력해 주세요.");
+
+    let mut winning_numbers = String::new();
+
+    io::stdin()
+        .read_line(&mut winning_numbers)
+        .expect("당첨 번호 읽기에 실패했습니다.");
+
+    println!("보너스 번호를 입력해 주세요.");
+
+    let mut bonus_number = String::new();
+
+    io::stdin()
+        .read_line(&mut bonus_number)
+        .expect("보너스 번호 읽기에 실패했습니다");
+
     println!("[DEBUG] 구입금액: {purchase_amount}");
+    println!("[DEBUG] 당첨 번호: {winning_numbers}");
+    println!("[DEBUG] 보너스 번호: {bonus_number}");
 }
