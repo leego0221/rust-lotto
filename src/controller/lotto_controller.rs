9da44bf0435bec81d5ebaecd0b6245b892b3ccf1
leego0221 <leego0221@naver.com>
2025-11-20 17:07:56 +0900

    refactor: 입력 로직 메소드 추출

--- src/controller/lotto_controller.rs
+++ src/controller/lotto_controller.rs
@@ -15,92 +15,95 @@ impl LottoController {
     }
 
     pub fn run(&self) {
-        // 구입금액 입력
-        let purchase_amount = loop {
-            let input = input_view::read_purchase_amount();
-            let value = input_parser::parse_unsigned_integer(&input);
+        let purchase_amount = Self::read_purchase_amount();
 
-            match value {
-                Ok(value) => match PurchaseAmount::new(value) {
-                    Ok(value) => break value,
-                    Err(e) => {
-                        eprintln!("{} 다시 입력해주세요.", e.message());
-                        continue;
-                    },
-                },
-                Err(e) => {
-                    eprintln!("{}", e.message());
-                    continue;
-                },
-            };
-        };
-
-        // 구입 금액에 해당하는 만큼 로또 발행하기
         let lottos = lotto_service::purchase(&purchase_amount);
-
-        // 구매 개수, 구매한 로또 리스트 출력
         output_view::show_purchase_count(lottos.len());
         output_view::show_purchased_lottos(&lottos);
 
-        // 당첨 번호 입력
-        let winning_numbers = loop {
-            let input = input_view::read_winning_numbers();
-            let value = input_parser::parse_winning_number(&input);
+        let winning_numbers = Self::read_winning_numbers();
+        let bonus_number = Self::read_bonus_number(&winning_numbers);
 
-            match value {
-                Ok(value) => match WinningNumbers::new(value) {
-                    Ok(value) => break value,
-                    Err(e) => {
-                        eprintln!("{} 다시 입력해주세요.", e.message());
-                        continue;
-                    }
-                },
-                Err(e) => {
-                    eprintln!("{} 다시 입력해주세요.", e.message());
-                    continue;
-                },
-            };
-        };
-
-        // 보너스 번호 입력
-        let bonus_number = loop {
-            let input = input_view::read_bonus_number();
-            let value = input_parser::parse_unsigned_integer(&input);
-
-            let number = match value {
-                Ok(value) => match BonusNumber::new(value) {
-                    Ok(value) => value,
-                    Err(e) => {
-                        eprintln!("{} 다시 입력해주세요.", e.message());
-                        continue;
-                    },
-                },
-                Err(e) => {
-                    eprintln!("{} 다시 입력해주세요.", e.message());
-                    continue;
-                }
-            };
-
-            // 당첨 번호와 보너스 번호 중복 검증
-            match lotto_service::check_duplicate(&winning_numbers, &number) {
-                Ok(()) => break number,
-                Err(e) => {
-                    eprintln!("{} 다시 입력해주세요.", e.message());
-                    continue;
-                }
-            };
-        };
-
-        // 번호 일치 여부에 따라 등수 매긴 뒤 내부에 저장하기
         let mut lotto_rank_service = LottoRankService::new();
         lotto_rank_service.determine_ranks(&lottos, &winning_numbers, &bonus_number);
+
         let rank_counter = lotto_rank_service.get_rank();
-
-        // 총 구매 금액에 따른 수익률 계산하기
         let profit_rate = lotto_rank_service.calculate_profit_rate(&purchase_amount);
-
-        // 로또 순위 집계표 및 수익률 출력하기
         output_view::show_winning_statistics(&rank_counter);
         output_view::show_profit_rate(profit_rate);
+    }
+
+    fn read_purchase_amount() -> PurchaseAmount {
+        loop {
+            let input_value = input_view::read_purchase_amount();
+            
+            let parsed_value = match input_parser::parse_unsigned_integer(&input_value) {
+                Ok(v) => v,
+                Err(e) => {
+                    eprintln!("{} 다시 입력해주세요.", e.message());
+                    continue;
+                },
+            };
+
+            match PurchaseAmount::new(parsed_value) {
+                Ok(v) => break v,
+                Err(e) => {
+                    eprintln!("{} 다시 입력해주세요.", e.message());
+                    continue;
+                },
+            }
+        }
+    }
+
+    fn read_winning_numbers() -> WinningNumbers {
+        loop {
+            let input_value = input_view::read_winning_numbers();
+
+            let parsed_value = match input_parser::parse_winning_number(&input_value) {
+                Ok(v) => v,
+                Err(e) => {
+                    eprintln!("{} 다시 입력해주세요.", e.message());
+                    continue;
+                },
+            };
+
+            match WinningNumbers::new(parsed_value) {
+                Ok(v) => break v,
+                Err(e) => {
+                    eprintln!("{} 다시 입력해주세요.", e.message());
+                    continue;
+                },
+            }
+        }
+    }
+
+    fn read_bonus_number(winning_numbers: &WinningNumbers) -> BonusNumber {
+        loop {
+            let input = input_view::read_bonus_number();
+
+            let value = match input_parser::parse_unsigned_integer(&input) {
+                Ok(v) => v,
+                Err(e) => {
+                    eprintln!("{} 다시 입력해주세요.", e.message());
+                    continue;
+                }
+            };
+
+            let bonus_number = match BonusNumber::new(value) {
+                Ok(v) => v,
+                Err(e) => {
+                    eprintln!("{} 다시 입력해주세요.", e.message());
+                    continue;
+                }
+            };
+
+            match lotto_service::check_duplicate(winning_numbers, &bonus_number) {
+                Ok(()) => break bonus_number,
+                Err(e) => {
+                    eprintln!("{} 다시 입력해주세요.", e.message());
+                    continue;
+                }
+            }
+        }
     }
 }
